joeusercz/OSCam-with-emu-branch

commit c663a0365ef09d8d4ee713a5dfa2db105d70e7fe

[gbx] - fix warning:[-Wmisleading-indentation]
git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11311 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-gbox.c
@@ -683,11 +683,14 @@ int32_t gbox_cmd_hello_rcvd(struct s_client *cli, uchar *data, int32_t n)
 					gbox_peer_online(peer, GBOX_PEER_ONLINE);
 				}
 				else
+				{
 					if (cfg.log_hello)
 						{ cs_log("-> HelloS from %s (%s:%d) V2.%02X with %d cards", cli->reader->label, cs_inet_ntoa(cli->ip), cli->reader->r_port, peer->gbox.minor_version, peer->filtered_cards); }
 					else
 						{	cs_log_dbg(D_READER,"-> HelloS in %d packets from %s (%s:%d) V2.%02X with %d cards filtered to %d cards", (data[0x0B] & 0x0f)+1, cli->reader->label, cs_inet_ntoa(cli->ip), cli->reader->r_port, peer->gbox.minor_version, peer->total_cards, peer->filtered_cards); }
+						
 					gbox_send_hello(cli, GBOX_STAT_HELLOR);
+				}
 			}
 			else
 			{

commit 0a30f8bfaf89838069b5dab26887d4fff8128d80

[gbx] - fixed bug introduced with r11311
git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11336 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-gbox.c
@@ -688,9 +688,8 @@ int32_t gbox_cmd_hello_rcvd(struct s_client *cli, uchar *data, int32_t n)
 						{ cs_log("-> HelloS from %s (%s:%d) V2.%02X with %d cards", cli->reader->label, cs_inet_ntoa(cli->ip), cli->reader->r_port, peer->gbox.minor_version, peer->filtered_cards); }
 					else
 						{	cs_log_dbg(D_READER,"-> HelloS in %d packets from %s (%s:%d) V2.%02X with %d cards filtered to %d cards", (data[0x0B] & 0x0f)+1, cli->reader->label, cs_inet_ntoa(cli->ip), cli->reader->r_port, peer->gbox.minor_version, peer->total_cards, peer->filtered_cards); }
-						
-					gbox_send_hello(cli, GBOX_STAT_HELLOR);
 				}
+			gbox_send_hello(cli, GBOX_STAT_HELLOR);
 			}
 			else
 			{